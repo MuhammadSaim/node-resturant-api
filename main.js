1170b8e741e694902e1f35f8f85257f31f4ec405
Muhammad Saim <muhammadsaim494@gmail.com>
2023-05-08 14:13:32 +0500

    fix CROS issue

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
-
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // register routes
 app.use("/api", require("./routes/api.js"));
